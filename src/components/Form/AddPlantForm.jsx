93d99f5dcf7e484545b9b2b492898debfa60a861
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-12-31 20:58:19 +0600

    feat: add upload button text state management in AddPlantForm component

--- src/components/Form/AddPlantForm.jsx
+++ src/components/Form/AddPlantForm.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
 
-const AddPlantForm = ({ handleSubmit }) => {
+const AddPlantForm = ({
+  handleSubmit,
+  uploadButtonText,
+  setUploadButtonText,
+}) => {
   return (
     <div className="w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50">
       <form onSubmit={handleSubmit}>
@@ -89,6 +93,9 @@ const AddPlantForm = ({ handleSubmit }) => {
                 <div className="flex flex-col w-max mx-auto text-center">
                   <label>
                     <input
+                      onChange={(e) => {
+                        setUploadButtonText(e.target.files[0].name);
+                      }}
                       className="text-sm cursor-pointer w-36 hidden"
                       type="file"
                       name="image"
@@ -97,7 +104,7 @@ const AddPlantForm = ({ handleSubmit }) => {
                       hidden
                     />
                     <div className="bg-lime-500 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-lime-500">
-                      Upload
+                      {uploadButtonText}
                     </div>
                   </label>
                 </div>
