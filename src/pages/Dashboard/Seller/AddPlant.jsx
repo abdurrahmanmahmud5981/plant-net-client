3c463d0a609f19bd98fa3f86940f7f6ee3637739
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-12-31 20:39:17 +0600

    feat: implement AddPlantForm with form submission handling and prop validation

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -2,6 +2,21 @@ import { Helmet } from 'react-helmet-async'
 import AddPlantForm from '../../../components/Form/AddPlantForm'
 
 const AddPlant = () => {
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const plant = {
+      name: formData.get('name'),
+      category: formData.get('category'),
+      description: formData.get('description'),
+      price: Number(formData.get('price')),
+      quantity: Number(formData.get('quantity')),
+      image: formData.get('image'),
+    }
+
+    // Send plant data to Firebase
+    // ...
+  }
   return (
     <div>
       <Helmet>
@@ -9,7 +24,7 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm handleSubmit={handleSubmit} />
     </div>
   )
 }
