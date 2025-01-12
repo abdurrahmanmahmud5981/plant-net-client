6d0fe1f8f26f17bfe413f61a0e6e2c2bdac42d72
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-01-12 09:33:44 +0600

    fix: update role fetching logic to ensure user email is valid and improve user info handling in AuthProvider

--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -1,18 +1,19 @@
 import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 
 const useRole = () => {
   const axiosSecure = useAxiosSecure();
-  const { user } = useAuth();
-  const {data:role, isLoading} = useQuery({
-    queryKey: ['role', user?.email],
+  const { user, loading } = useAuth();
+  const { data: role, isLoading } = useQuery({
+    queryKey: ["role", user?.email],
+    enabled: !loading && !!user?.email,
     queryFn: async () => {
-        const {data} =  await axiosSecure(`/users/role/${user?.email}`);
-        return data;
-    }
-  })
-  return [role, isLoading]
+      const { data } = await axiosSecure(`/users/role/${user?.email}`);
+      return data;
+    },
+  });
+  return [role, isLoading];
 };
 
 export default useRole;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -64,7 +64,7 @@ const AuthProvider = ({ children }) => {
           role: "customer",
         });
         // // save user information in database
-        if (user?.displayName && user?.photoURL) {
+        if (currentUser?.displayName && currentUser?.photoURL) {
           await axios.post(
             `${import.meta.env.VITE_API_URL}/users/${currentUser.email}`,
             {
